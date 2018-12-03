afb769d36224426290824cc0e0751cfb1e2169fa
Dylan <pascuad21@up.edu>
2018-12-03 14:59:18 -0800

    Gave the robot a facelife. Added myself to the author list.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw, Nathan Schmedake
+ * @author Tanya L. Crenshaw, Nathan Schmedake, Dylan Pascua
  * @since August 2013
  *
  */
@@ -55,15 +55,15 @@ void dalekPrintAscii(void)
   printf("\n\n"
 	 "I am Dalek, from Dr. Who\n"
 	 "%s      <----->      \n" 
-         "%s     <  (0)  >     \n"        
+         "%s     <  (@)  >     \n"        
          "%s     |       |     \n"
          "%s    < ------- >    \n"
-         "%s    o         o    \n"
-         "%s    o  0  ()  o    \n"
-	 "%s   o           o   \n"
-   	 "%s o o o o o o o o o \n"
-   	 "%s o o o o o o o o o \n"
-   	 "%s o o o o o o o o o \n", blank, blank, blank, blank, blank, blank, 
+         "%s    @         @    \n"
+         "%s    @  0  ()  @   \n"
+	 "%s   @           @   \n"
+   	 "%s @ o o o o o o o @ \n"
+   	 "%s @ o o o o o o o @ \n"
+   	 "%s @ @ @ @ @ @ @ @ @ \n", blank, blank, blank, blank, blank, blank, 
 	 blank, blank, blank, blank);
   blank = blank - 10;
 }
